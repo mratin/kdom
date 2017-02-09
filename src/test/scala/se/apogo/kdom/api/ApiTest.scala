377f10e827f3006e41ad2d6cedf517f964985265
Martin Butler <martin.butler@tomologic.com>
2017-02-09 14:41:27 +0100

    Add some basic heuristic to AI players in ApiTest

--- src/test/scala/se/apogo/kdom/api/ApiTest.scala
+++ src/test/scala/se/apogo/kdom/api/ApiTest.scala
@@ -7,8 +7,6 @@ import org.json4s.jackson.JsonMethods._
 import org.testng.annotations.Test
 import model._
 
-import scala.util.Random
-
 class ApiTest {
   val apiUrl = "http://localhost:8080"
 
@@ -42,7 +40,7 @@ class ApiTest {
       } else {
         val availableMoves: Moves = parse(http(GET(s"${apiUrl}/games/${gameId}/available-moves")).entityAsString).extract[Moves]
 
-        val chosenMove: Move = availableMoves.moves(Random.nextInt(availableMoves.moves.size))
+        val chosenMove: Move = availableMoves.moves.min(movePriority)
 
         val currentPlayerToken = joinedPlayers.find(_.name == game.currentPlayer.get.name).head
 
@@ -55,4 +53,42 @@ class ApiTest {
 
     print(response)
   }
+
+  /**
+    * @return simple ordering of moves, to get some intelligence when choosing moves
+    */
+  def movePriority: Ordering[Move] = {
+    def positionScalar(position: Position): Int = math.abs(position.col) + math.abs(position.row)
+    def placedScalar(placedDomino: PlacedDomino): Int = {
+      positionScalar(placedDomino.tile1Position) + positionScalar(placedDomino.tile2Position)
+    }
+    def crowns(domino: Domino): Int = domino.tile1.crowns + domino.tile2.crowns
+
+    (x: Move, y: Move) => {
+      (x.chosenDomino, y.chosenDomino) match {
+        case (Some(xChosen), Some(yChosen)) => {
+
+          // Choose the domino with the most crowns:
+          val chosenCrownCmp: Int = crowns(xChosen).compareTo(crowns(yChosen))
+          if (chosenCrownCmp != 0) chosenCrownCmp else {
+
+            // Choose the domino with the lowest number:
+            val chosenNumberCmp: Int = xChosen.number.compareTo(yChosen.number)
+            if (chosenNumberCmp != 0) chosenNumberCmp else {
+
+              // Choose the position closest to the castle
+              (x.placedDomino, y.placedDomino) match {
+                case (Some(xPlaced), Some(yPlaced)) =>
+                  placedScalar(xPlaced).compareTo(placedScalar(yPlaced))
+                case _ => 0
+              }
+            }
+          }
+
+        }
+        case _ => 0
+      }
+    }
+  }
+
 }
